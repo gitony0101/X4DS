ccc90cb478b37fc9fb39d8a71fe07a5574759f3f
gitony0101 <53283254+gitony0101@users.noreply.github.com>
2024-06-05 21:26:00 -0300

    update
    Signed-off-by: gitony0101 <53283254+gitony0101@users.noreply.github.com>

--- Projects/scrapping_js/index.js
+++ Projects/scrapping_js/index.js
@@ -1,3 +1,48 @@
-import axios from 'axios';
-import cheerio from 'cheerio';
+import puppeteer from 'puppeteer';
+import { load } from 'cheerio';
 import express from 'express';
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(
+    'https://oreganstoyotabridgewater.com/inventory/?do-search=1&search.vehicle-inventory-type-ids.0=1',
+    { waitUntil: 'networkidle2' },
+  );
+
+  // 等待特定元素加载
+  await page.waitForSelector('.ouvsrItem');
+
+  const htmlData = await page.content();
+  await browser.close();
+
+  const $ = load(htmlData);
+  const carInfo = [];
+
+  $('.ouvsrItem').each((index, element) => {
+    const carModel = $(element).find('.ouvsrModelYear').text().trim();
+    const carPrice = $(element).find('.currencyValue').text().trim();
+    const carSpecs = [];
+
+    $(element)
+      .find('.ouvsrTechSpecs .ouvsrSpec')
+      .each((i, specElement) => {
+        const label = $(specElement).find('.ouvsrLabel').text().trim();
+        const value = $(specElement).find('.ouvsrValue').text().trim();
+        carSpecs.push({ label, value });
+      });
+
+    carInfo.push({
+      carModel,
+      carPrice,
+      carSpecs,
+    });
+  });
+
+  console.log('Car Info: ', JSON.stringify(carInfo, null, 2));
+})().catch((err) => console.error(err));
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
